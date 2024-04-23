669b06e4b54115574557c0d1f8a51066a7b66fdd
Gabriela-Auanavasc <165970579+Gabriela-Auanavasc@users.noreply.github.com>
2024-04-23 19:37:50 -0300

    Update quem_ganhou.py

--- quem_ganhou.py
+++ quem_ganhou.py
@@ -14,45 +14,44 @@ def quem_ganhou():
         for linha in tabuleiro:
             for jogada in linha:
                 if jogada == ' ':
-                    resultado = 'o jogo ainda não acabou'
+                    resultado = 'o jogo ainda não acabou...'
 
         # verificar as linhas
         for linha in tabuleiro:
             if linha[0] == linha[1] == linha[2] and linha[0] != ' ':
-                resultado = linha[0]
+                resultado = f'o jogador {linha[0]} ganhou'
 
         # verificar colunas
         for coluna in [0, 1, 2]:
             if tabuleiro[0][coluna] == tabuleiro[1][coluna] == tabuleiro[2][coluna] and tabuleiro[0][coluna] != ' ':
-                resultado = tabuleiro[0][coluna]
+                resultado = f'o jogador {tabuleiro[0][coluna]} ganhou'
 
         # verificar cada posicao nas linhas do tabuleiro
         if tabuleiro[0][0] == tabuleiro[0][1] == tabuleiro[0][2] and tabuleiro[0][0] != ' ':
-            resultado = tabuleiro[0][0]
+            resultado = f'o jogador {tabuleiro[0][0]} ganhou'
         elif tabuleiro[1][0] == tabuleiro[1][1] == tabuleiro[1][2] and tabuleiro[1][0] != ' ':
-            resultado = tabuleiro[1][0]
+            resultado = f'o jogador {tabuleiro[1][0]} ganhou'
         elif tabuleiro[2][0] == tabuleiro[2][1] == tabuleiro[2][2] and tabuleiro[2][0] != ' ':
-            resultado = tabuleiro[2][0]
+            resultado = f'o jogador {tabuleiro[2][0]} ganhou'
 
         # colunas
         if tabuleiro[0][0] == tabuleiro[1][0] == tabuleiro[2][0] and tabuleiro[0][0] != ' ':
-            resultado = tabuleiro[0][0]
+            resultado = f'o jogador {tabuleiro[0][0]} ganhou'
         elif tabuleiro[1][0] == tabuleiro[1][1] == tabuleiro[2][1] and tabuleiro[1][0] != ' ':
-            resultado = tabuleiro[1][0]
+            resultado = f'o jogador {tabuleiro[1][0]} ganhou'
         elif tabuleiro[2][0] == tabuleiro[2][1] == tabuleiro[2][2] and tabuleiro[2][0] != ' ':
-            resultado = tabuleiro[2][0]
+            resultado = f'o jogador {tabuleiro[2][0]} ganhou'
 
         # diagonais
         # 00 == 11 == 22
         # 20 == 11 == 02
         if tabuleiro[0][0] == tabuleiro[1][1] == tabuleiro[2][2] and tabuleiro[0][0] != ' ':
-            resultado = tabuleiro[0][0]
+            resultado = f'o jogador {tabuleiro[0][0]} ganhou'
         elif tabuleiro[2][0] == tabuleiro[1][1] == tabuleiro[0][2] and tabuleiro[2][0] != ' ':
-            resultado = tabuleiro[2][0]
+            resultado = f'o jogador {tabuleiro[2][0]} ganhou'
 
         # empate:
         if resultado == '':
-            resultado = '> EMPATE'
+            resultado = 'deu empate'
 
     return resultado
-  
